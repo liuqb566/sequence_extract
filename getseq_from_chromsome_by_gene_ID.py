68b3708232cd3b7c5595091838c6254607794532
liuqb566 <liuqb566@gmail.com>
2020-01-03 20:09:37 +0800

    速度优化

--- getseq_from_chromsome_by_gene_ID.py
+++ getseq_from_chromsome_by_gene_ID.py
@@ -4,10 +4,15 @@
 #History:
 #   2020-1-3 v4 gossie
 
+#基因组文件
 genome='/home/liuqb/data/genome/Cotton/HAU1.0/HAU_genome.fa'
-gene='gene.txt'
+#注释文件，GFF3格式
 GFF='/home/liuqb/data/genome/Cotton/HAU1.0/HAU.gene.gff3'
+#基因 ID
+gene='gene.txt'
+#上游碱基数（默认从5‘UTR之前开始计算）
 up=2000
+#下游碱基数(默认从3’UTR之后开始计算)
 down=2000
 
 import re
@@ -19,18 +24,18 @@ with open(genome,'r') as fa:
         line = line.strip()
         if line.startswith('>'):
             chr_id = line[1:]
-            genome_dic[chr_id] = ''
+            genome_dic[chr_id] = []
         else:
-            genome_dic[chr_id] += line
-        print('dic ok')
+            genome_dic[chr_id].append(line)
+    for key,value in genome_dic.items():
+        genome_dic[key]=''.join(value)
 
-
-#定义函数，Reverse Complement
+#定义函数，Reverse Complement，对负链上的基因反向互补
 def rev_comp(seq):
      base_trans = {'A':'T', 'C':'G', 'T':'A', 'G':'C', 'a':'t', 'c':'g', 't':'a', 'g':'c'}
      rev_seq = list(reversed(seq))
      rev_comp_seq_list = [base_trans[k] for k in rev_seq]
-     rev_comp_seq = ''.join(rev_seq_list)
+     rev_comp_seq = ''.join(rev_comp_seq_list)
      return(rev_comp_seq)
 
 with open(gene,'r') as gene_list:
@@ -38,34 +43,28 @@ with open(gene,'r') as gene_list:
         geneid=geneid.strip()
         with open(GFF,'r') as gff:
             for line in gff:
-                line_list = line.strip().split()
-                if geneid in line_list[8] and line_list[2]=='gene' and line_list[7]=='+':
-                    chr=line_list[0]
+                line=line.strip()
+                line_list=line.split()
+                if not line.startswith("#") and geneid in line_list[8] and line_list[2]=='gene' and line_list[6]=='+':
                     gene_start=int(line_list[3])
                     gene_end=int(line_list[4])
                     up_start=int(int(line_list[3])-up)
                     down_end=int(int(line_list[4])+down)
-                    print('1ok')
-                    for key in genome_dic.keys():
-                        if key == chr:
-                            gene_sequence=genome_dic[key][gene_start-1:gene_end]
-                            up_stream=genome_dic[key][up_start-1:gene_start]
-                            down_stream=genome_dic[key][gene_end:down_end]
-                            up_stream=rev_comp(genome_dic[key][gene_end:down_end])
-                            print ('>%s\t%s-%s\t%s\n%s\n>%s\t%s-%s\t%s\n%s\n>%s\t%s-%s\t%s\n%s' %(geneid,gene_start,gene_end,line_list[7],gene_sequence,geneid+"_up",up_start,gene_start-1,line_list[7],up_stream,geneid+"_down",gene_end+1,down_end,line_list[7],down_stream))
-                elif geneid in line_list[8] and line_list[2]=='gene' and line_list[7]=='-':
-                    chr=line_list[0]
+                    chr_seq=genome_dic.get(line_list[0],"Chr name erro")	#字典的get()函数
+                    gene_sequence=chr_seq[gene_start-1:gene_end]
+                    up_stream=chr_seq[up_start-1:gene_start]
+                    down_stream=chr_seq[gene_end:down_end]
+                    print ('>%s %s-%s %s\n%s\n>%s %s-%s %s\n%s\n>%s %s-%s %s\n%s' %(geneid,gene_start,gene_end,line_list[6],gene_sequence,geneid+"_up",up_start,gene_start-1,line_list[6],up_stream,geneid+"_down",gene_end+1,down_end,line_list[6],down_stream))
+                    break 
+                elif not line.startswith("#") and geneid in line_list[8] and line_list[2]=='gene' and line_list[6]=='-':
                     gene_start=int(line_list[3])
                     gene_end=int(line_list[4])
                     down_start=int(int(line_list[3])-down) #负链基因的下游在左端
                     up_end=int(int(line_list[4])+up)	   #负链基因的上游在右端
-                    print('2ok')
-                    for key in genome_dic.keys():
-                        if key == chr:
-                            gene_sequence=rev_comp(genome_dic[key][gene_start-1:gene_end])	#负链上的序列都要反向互补
-                            down_stream=rev_comp(genome_dic[key][down_start-1:gene_start])
-                            up_stream=rev_comp(genome_dic[key][gene_end:down_end])
-                            print ('>%s\t%s-%s\t%s\n%s\n>%s\t%s-%s\t%s\n%s\n>%s\t%s-%s\t%s\n%s' %(geneid,gene_start,gene_end,line_list[7],gene_sequence,geneid+"_up",gene_end+1,up_end,line_list[7],up_stream,geneid+"_down",down_start,gene_start-1,line_list[7],down_stream))
-
-
+                    chr_seq=genome_dic.get(line_list[0],"Chr name erro")
+                    gene_sequence=rev_comp(chr_seq[gene_start-1:gene_end])	#负链上的序列都要反向互补
+                    down_stream=rev_comp(chr_seq[down_start-1:gene_start])
+                    up_stream=rev_comp(chr_seq[gene_end:up_end])
+                    print ('>%s %s-%s %s\n%s\n>%s %s-%s %s\n%s\n>%s %s-%s %s\n%s' %(geneid,gene_start,gene_end,line_list[6],gene_sequence,geneid+"_up",gene_end+1,up_end,line_list[6],up_stream,geneid+"_down",down_start,gene_start-1,line_list[6],down_stream))
+                    break
 
